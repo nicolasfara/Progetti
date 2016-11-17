57b676b169e0d70b24aa058466b863ce74d03445
AndreaGiulianini <andrea.giulianini97@gmail.com>
2016-11-17 13:36:54 +0100

    Aggiunta commenti

--- AndreaG/Consegne/Es1S8/main.c
+++ AndreaG/Consegne/Es1S8/main.c
@@ -1,13 +1,22 @@
+/*
+Nome Prop: Andrea Giulianini
+Nome Es: Es1S8
+Data Inizio:17/11/2016
+Data Fine:17/11/2016
+*/
 #include <stdio.h>
 #include <stdlib.h>
 
+
+
 int main()
 {
-    int a=1;
-    int *pa;
-    pa=&a;
-    printf("%d",*pa);
+    int a=1; //Dichiaro,Inizializzo Variabile
+    int *pa; //Dichiaro Vettore
+    pa=&a; //Inizializzo Puntatore
+    printf("%d",*pa); //Output
 
+    //Ripeto come prima
     char b='b';
     char *pb;
     pb=&b;
